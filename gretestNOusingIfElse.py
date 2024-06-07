9f313cfba2e8f1526788aa41222ca72168a02d75
AayushRathour <153987336+AayushRathour@users.noreply.github.com>
2024-06-07 22:49:52 +0530

    Add files via upload

---
+++ gretestNOusingIfElse.py
@@ -0,0 +1,10 @@
+a =int(input("enter fist no.."))
+b=int(input("enter the second no.."))
+c=int(input("enter the second n0.."))
+
+if a>b and a>c:
+    print(a,"is greter")
+elif b>a and b>c:
+    print(b,"is greter.")
+elif c>a and c>b:
+    print(c,"is greater  ")        
